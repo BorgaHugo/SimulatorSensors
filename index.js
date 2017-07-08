0fe91a4eef55d3860e0e99a96009c5118a0a4379
Hugo Borga <hugo.miguel.borga@gmail.com>
2017-07-08 20:00:33 +0100

    include add gitignore 2

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+'use strict';
+/**
+ * Module dependencies.
+ */
+
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .command('start [optional]')
+  .description('Begins start send data sensors.')
+  .option('-a, --all','Send all parameters sensores in package.')
+  .option('-l, --long','')
+  .action(simulateData);
+program.parse(process.argv);
